e2b22a905e7879ca14a7f294cfb8b204b6982b47
Famy9 <ernis701@gmail.com>
2018-01-31 16:45:20 +0200

    pakeista

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 
 public class Main {
     private int Skaiciai;
-    private int[] mas= new int[Skaiciai];
 
     public static void main(String[] args) {
         Main objektas = new Main();
@@ -14,14 +13,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.print("Iveskite kiek norite irasyti skaiciu: ");
         Skaiciai = sc.nextInt();
-        mas = new int[Skaiciai];
+        int[] mas = new int[Skaiciai];
         for (int i=0; i<mas.length; i++){
             System.out.println("Iveskite " + i + "-a skaiciu: ");
             mas[i]=sc.nextInt();
         }
-        Isvedimas();
-    }
-    public void Isvedimas(){
         System.out.println("Didesni nei 100 skaiciai yra: ");
         for (int i=0; i<mas.length; i++){
             if (mas[i]>100){
