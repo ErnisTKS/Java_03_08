fa02850339cd094c1844c6837e95526c62356a1a
Famy9 <ernis701@gmail.com>
2018-01-31 16:28:47 +0200

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+    private int Skaiciai;
+    private int[] mas= new int[Skaiciai];
+
+    public static void main(String[] args) {
+        Main objektas = new Main();
+        objektas.Ivedimas();
+    }
+
+    public void Ivedimas(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Iveskite kiek norite irasyti skaiciu: ");
+        Skaiciai = sc.nextInt();
+        mas = new int[Skaiciai];
+        for (int i=0; i<mas.length; i++){
+            System.out.println("Iveskite " + i + "-a skaiciu: ");
+            mas[i]=sc.nextInt();
+        }
+        Isvedimas();
+    }
+    public void Isvedimas(){
+        System.out.println("Didesni nei 100 skaiciai yra: ");
+        for (int i=0; i<mas.length; i++){
+            if (mas[i]>100){
+                System.out.println(i + " vietoje esantis skaicius: "+mas[i]);
+            }
+        }
+    }
+}
